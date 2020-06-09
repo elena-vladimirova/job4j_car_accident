ec87ccf3f8b6e86cd0cd904c7f0e84eb3e6c32c2
elena-vladimirova <avtobusova@mail.ru>
2020-06-09 21:18:15 +0300

    Что такое DI?[6876#303563]

---
+++ src/main/java/ru/job4j/di/Context.java
@@ -0,0 +1,48 @@
+package ru.job4j.di;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Context {
+    private Map<String, Object> els = new HashMap<String, Object>();
+
+    public void reg(Class cl) {
+        Constructor[] constructors = cl.getDeclaredConstructors();
+        if (constructors.length > 1) {
+            throw new IllegalStateException("Class has multiple constructors : " + cl.getCanonicalName());
+        }
+        Constructor con = constructors[0];
+        List<Object> args = new ArrayList<Object>();
+        for (Class arg : con.getParameterTypes()) {
+            if (!els.containsKey(arg.getCanonicalName())) {
+                throw new IllegalStateException("Object doesn't found in context : " + arg.getCanonicalName());
+            }
+            args.add(els.get(arg.getCanonicalName()));
+        }
+        try {
+            els.put(cl.getCanonicalName(), con.newInstance(args.toArray()));
+        } catch (Exception e) {
+            throw new IllegalStateException("Coun't create an instance of : " + cl.getCanonicalName(), e);
+        }
+    }
+
+    public <T> T get(Class<T> inst) {
+        return (T) els.get(inst.getCanonicalName());
+    }
+
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.reg(Store.class);
+        context.reg(StartUI.class);
+
+        StartUI ui = context.get(StartUI.class);
+
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+    }
+
+}
