4e73c6999605e8df7f1780900d24c8ee603e73c1
elena-vladimirova <avtobusova@mail.ru>
2020-06-14 16:11:34 +0300

    Загрузка данных в вид[295782#303555]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -1,13 +1,18 @@
 package ru.job4j.accident.control;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("user", "Elena");
+        model.addAttribute("numbers", List.of("1", "2", "3"));
         return "index";
     }
 }
